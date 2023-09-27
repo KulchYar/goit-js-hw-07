dc0df52bacf39bb78ecce2e8538bfab4b91cce06
KulchYar <108682555+KulchYar@users.noreply.github.com>
2023-09-27 15:38:11 +0300

    1.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery")
+
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+list.addEventListener("click", handleClick)
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+    </a>
+    </li>
+    `).join("")
+}
+
+function handleClick(event) {
+    event.preventDefault();
+    if (event.target === event.currentTarget) {
+        return
+    }
+    const largeImage = event.target.dataset.source;
+    const imageDescription = event.target.alt
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+       <img src="${largeImage}" alt="${imageDescription}"
+    </div>
+`)
+
+instance.show()
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery")
+
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+list.addEventListener("click", (event) =>  {
+    event.preventDefault();
+})
+
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `).join("")
+}
+
+
+
+    const gallery = new SimpleLightbox(".gallery a", {
+        captions: true,
+        captionDelay: 250,
+        captionsData: "alt",
+    }
+    )
